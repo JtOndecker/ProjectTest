7602ad4bc9a9a5248491502b2d172e5a6cb3fc6c
JtOndecker <jtondecker@gmail.com>
2018-01-21 20:29:37 -0500

    testing

--- app/src/main/java/com/example/jason/projecttest/MainActivity.java
+++ app/src/main/java/com/example/jason/projecttest/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-// test change
+// test changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
